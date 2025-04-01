0ce8b16fe4b110b908779b8c6aa902dbe6d47e8e
ygor emmanoel <ygoremmanoel23@gmail.com>
2025-04-01 09:18:30 -0300

    atualização do exercicio anterior
    git

--- Arquivo01.py
+++ Arquivo01.py
@@ -1,5 +1,16 @@
 nome=str(input("Digite seu nome :"))
 idade=int(input("Digite sua idade :"))
 salario=float(input("Digite seu salario:"))
+p=float(input("o percentual de seu salario é :"))
+s= salario * p  /100
+aumemto = salario+s
+print(f"Ola {nome}.\n Sua idade é {idade} anos.\n seu salario é de {salario}.\n"
+      f"seu aumento de salario foi de {s}.\nseu novo salario é {aumemto}")
 
-print(f"Ola {nome}.\n Sua idade é {idade} anos.\n seu salario é de {salario}")
+
+
+
+
+
+
+
