3444b0e16a2c5c108fca3c3bfb7882c00ba21a19
ygor emmanoel <ygoremmanoel23@gmail.com>
2025-04-01 09:30:10 -0300

    nova  atividade add

---
+++ Arquivo02.py
@@ -0,0 +1,10 @@
+n1=float(input("Digite qualquer numero: "))
+n2=float(input("Digite qualquer numero: "))
+
+soma=n1+n2
+sub=n1-n2
+mul=n1*n2
+div=n1/n2
+
+print(f"sua soma foi {soma}.\n sua subtração foi de {sub}.\n sua multiplicação foi de {mul}.\n"
+      f"sua divisão foi de {div}.")
