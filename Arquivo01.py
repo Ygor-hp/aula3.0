6659cac942e9a9da7bcdbcb2094143ad105db5ec
ygor emmanoel <ygoremmanoel23@gmail.com>
2025-04-01 08:53:23 -0300

    primeiro exercicio do dia

---
+++ Arquivo01.py
@@ -0,0 +1,5 @@
+nome=str(input("Digite seu nome :"))
+idade=int(input("Digite sua idade :"))
+salario=float(input("Digite seu salario:"))
+
+print(f"Ola {nome}.\n Sua idade é {idade} anos.\n seu salario é de {salario}")
